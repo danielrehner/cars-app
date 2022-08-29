2ab310f1040142b51acfbbfcc68768c4af05f74a
Daniel Rehner <danielrehner@gmail.com>
2022-08-29 12:51:20 -0500

    Add seed data (sourced from https://github.com/mixandgo/hotwire_dropdowns)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Data found here: https://github.com/abhionlyone/us-car-models-data
+require "csv"
+
+Dir.glob("#{Rails.root}/db/seed_data/*.csv") do |file|
+  next if file == '.' or file == '..'
+
+  CSV.read(file, headers: true).each do |row|
+    make = VehicleMake.find_or_create_by!(name: row["make"])
+    model = make.vehicle_models.find_or_create_by!(name: row["model"], vehicle_make: make)
+    year = VehicleYear.find_or_create_by!(name: row["year"], vehicle_model: model, vehicle_make: make)
+  end
+end
